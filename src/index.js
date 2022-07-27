37e78ad2c12dab5db7251571b7c0d03fd926b754
kandelseik <iraklikandelaki2@gmail.com>
2022-07-27 21:58:05 +0400

    The project is finished.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ import App from "./components/App";
 
 // importing Redux
 import { Provider } from "react-redux";
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
-import reduxThunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+
+// Importing Persistors
+// This is needed to persist the state on page reload
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Importing reducer
 import reducer from "./reducers";
 
-// Configuring store
-const store = configureStore({ reducer }, applyMiddleware(reduxThunk));
+// Creating a persisted store to persist state on page reload
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
